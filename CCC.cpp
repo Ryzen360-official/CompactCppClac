a6d3e07bdc8679f176eb4e7aa9c666b2afa6a1c4
RyzenMC <yashyt6246@gmail.com>
2026-02-15 17:32:33 +0530

    Update CCC.cpp

--- CCC.cpp
+++ CCC.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
 #include<cmath>
+void mean() {
+double a[100];
+int n = 0;
+while(n<100) {std::cout<<"Enter Number:";if(!(std::cin>>a[n])) {std::cout<<"Done.\n";break;}else if (n== 'q') {break;}n++;}if (n == 0) return;double mean = 0;for(int i=0;i<n;i++) mean +=a[i];std::cout<<"mean:"<< mean/n <<std::endl;}
 int main() {
     double IntegerI; double IntegerII;
     char calcoperator;
@@ -27,8 +31,12 @@ int main() {
         case '^' :
         std::cout<<"Enter Second Number:"; std::cin>>IntegerII; std::cout<<"Result: "<<pow(IntegerI,IntegerII);
         break;
+        case 'T' :
+        mean();
+        break;
         default:
         std::cout<<"Invalid.";
         break;
     }
 }
+//the test section is purely for testing purposes of different arithemetic operations that are to be implemented in this calculator
